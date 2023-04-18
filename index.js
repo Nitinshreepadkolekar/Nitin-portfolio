c9a8182ecd7af11950a5ddf21ff1bfd3b2a34cb8
Nitin sk <nitinskolekar@gmail.com>
2023-04-18 20:25:53 +0530

    second commit

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ app.use(express.static("public"))
 app.get("/",function(req,res){
   res.render("index")
 })
+app.get("/home",function(req,res){
+  res.render("home")
+})
+app.get("/contact",function(req,res){
+  res.render("contact")
+})
+app.get("/Resume",function(req,res){
+  res.render("resume")
+})
 app.listen(3000,function(){
   console.log("running on port 3000")
 })
